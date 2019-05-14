e14df8dd9e9d25054c89f85a51d7917414aa28fa
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-05-14 11:59:28 -0600

    deleted i3blocks-scripts

--- i3blocks-scripts/cpu.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-import psutil
-perc=int(psutil.cpu_percent(interval=1))
-print('<span font="Font Awesome">\uf0ae</span>{:>3}%'.format(perc))
--- i3blocks-scripts/mem.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-import psutil
-perc=int(psutil.virtual_memory().percent)
-print('<span font="Font Awesome">\uf2db</span>{:>3}%'.format(perc))
