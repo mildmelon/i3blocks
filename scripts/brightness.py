46c4604feea4cf50e2094b06ba555bd9108ef29d
Caidan Williams <9907093+mildmelon@users.noreply.github.com>
2020-08-21 14:00:00 -0400

    Update brightness.py

--- scripts/brightness.py
+++ scripts/brightness.py
@@ -2,16 +2,22 @@
 # Custom i3blocks python script for displaying current screen brightness
 #
 # Those are the brightness paths on my laptop, change it properly
-######################################################################
-brighPath = '/sys/class/backlight/intel_backlight/actual_brightness' #
-maxBrighPath = '/sys/class/backlight/intel_backlight/max_brightness' #
-######################################################################
+
 import math
-file=open(brighPath)
-brightness = int(file.read());
-file.close()
-file=open(maxBrighPath)
-maxBrightness = int(file.read())
-file.close()
-brightnessPerc = math.ceil(brightness * 100 / maxBrightness)
-print('<span font="FontAwesome">\uf185</span>{:>3}%'.format(brightnessPerc))
+
+# Edit these to match the paths on your machine
+bright_path = '/sys/class/backlight/intel_backlight/actual_brightness'
+max_bright_path = '/sys/class/backlight/intel_backlight/max_brightness'
+
+# You can see the icon here: https://fontawesome.com/icons/sun?style=solid
+icon = '<span font="FontAwesome">\uf185</span>'
+
+
+with open(bright_path) as file:
+    brightness = int(file.read())
+
+with open(max_bright_path) as file:
+    max_brightness = int(file.read())
+
+brightness_perc = math.ceil(brightness * 100 / max_brightness)
+print(f'{icon}{brightness_perc:>3}%')
